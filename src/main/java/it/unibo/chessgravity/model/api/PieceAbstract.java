5d9add363602cc58793e668edcaf995d2176af0e
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-08-29 19:33:11 +0200

    add PieceBastract

---
+++ src/main/java/it/unibo/chessgravity/model/api/PieceAbstract.java
@@ -0,0 +1,16 @@
+package it.unibo.chessgravity.model.api;
+
+/**
+ * Classe astratta utilizzata per raggruppare le implementazioni
+ * comuni dei 
+ */
+
+public abstract class PieceAbstract implements Piece {
+    private Board board;
+    private Square square;
+
+    public PieceAbstract(Board board, Square square) {
+        this.board = board;
+        this.square = square;
+    }
+}
