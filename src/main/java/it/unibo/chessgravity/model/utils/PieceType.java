b198cd95874e1d1da3dbdb9eda69d38531f11dbd
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-08-30 11:51:21 +0200

    update PieceType

--- src/main/java/it/unibo/chessgravity/model/utils/PieceType.java
+++ src/main/java/it/unibo/chessgravity/model/utils/PieceType.java
@@ -1,5 +1,21 @@
 package it.unibo.chessgravity.model.utils;
 
+import static it.unibo.chessgravity.model.utils.PieceGroup.*;
+
 public enum PieceType {
-    KING, QUEEN, ROOK, BISHOP, KNIGHT;
+    KING(STANDARD), 
+    QUEEN(STANDARD),
+    ROOK(STANDARD),
+    BISHOP(STANDARD),
+    KNIGHT(STANDARD);
+
+    private final PieceGroup group;
+
+    private PieceType(final PieceGroup group) {
+        this.group = group;
+    }
+
+    public PieceGroup getGroup() {
+        return this.group;
+    }
 }
