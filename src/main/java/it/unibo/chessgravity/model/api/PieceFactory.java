6e40d1c969e8353cdaa539c10f5d9a929f908298
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-08-30 11:47:35 +0200

    change PieceAbstract into PieceImpl

--- src/main/java/it/unibo/chessgravity/model/api/PieceFactory.java
+++ src/main/java/it/unibo/chessgravity/model/api/PieceFactory.java
@@ -1,9 +1,11 @@
 package it.unibo.chessgravity.model.api;
 
+import it.unibo.chessgravity.model.utils.PieceType;
+
 /**
  * Interfaccaia per applicare il factory method dei pezzi.
  */
 
 public interface PieceFactory {
-    Piece creatPiece();
+    Piece creatPiece(final PieceType type);
 }
---
+++ src/main/java/it/unibo/chessgravity/model/impl/PieceImpl.java
@@ -0,0 +1,34 @@
+package it.unibo.chessgravity.model.impl;
+
+import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.Piece;
+import it.unibo.chessgravity.model.api.Square;
+
+/**
+ * Implementazione dell'interfaccia Piece
+ */
+
+public class PieceImpl implements Piece {
+    protected Square square;
+    protected final Board board;
+    
+    public PieceImpl(final Board board, final Square square) {
+        this.board = board;
+        this.square = square;
+    }
+
+    @Override
+    public Square getSquare() {
+        return this.square;
+    }
+
+    protected void setSquare(final Square square) {
+        this.square = square;
+    }
+
+    @Override
+    public boolean move(Board board, Square destSquare) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
+}
