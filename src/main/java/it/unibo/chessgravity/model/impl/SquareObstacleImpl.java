a9d3f01ef27ec322650f8003f6980e4e8ea60eb9
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-01 11:32:56 +0200

    add SquareObstacleImpl class

---
+++ src/main/java/it/unibo/chessgravity/model/impl/SquareObstacleImpl.java
@@ -0,0 +1,22 @@
+package it.unibo.chessgravity.model.impl;
+
+import it.unibo.chessgravity.model.api.square.SquareObstacle;
+
+/**
+ * Implementazione dell'interfaccia SquareObstacle.
+ * Lo stato della classe sarà analogo per tutte le
+ * sue istanze. Non vi è possibilità di applicare
+ * modifiche
+ * La classe quindi funge da "place holder" per indicare
+ * quale casa deve prendere le sembianze di un'ostacolo.
+ */
+
+public class SquareObstacleImpl implements SquareObstacle {
+
+    @Override
+    public void setObstacle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setObstacle'");
+    }
+    
+}
