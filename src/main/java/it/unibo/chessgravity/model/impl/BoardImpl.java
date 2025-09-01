1089983cf56f6d4b3ec4f6547f4ba76907d1f8ca
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-01 17:11:59 +0200

    add BoardImpl constructor

--- src/main/java/it/unibo/chessgravity/model/impl/BoardImpl.java
+++ src/main/java/it/unibo/chessgravity/model/impl/BoardImpl.java
@@ -1,6 +1,10 @@
 package it.unibo.chessgravity.model.impl;
 
+import java.util.*;
+
 import it.unibo.chessgravity.model.api.*;
+import it.unibo.chessgravity.model.api.square.Square;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
 
 /**
  * Implementazione dell'interfaccia Board.
@@ -10,6 +14,19 @@ import it.unibo.chessgravity.model.api.*;
  */
 
 public class BoardImpl implements Board {
+    /*
+     * squareList is a List that contains a List<Square>. All the Squares in
+     * the List<Square> are the whole squares that will compose one board's row
+     */
+    private final List<List<Square>> squareList;
+
+    public BoardImpl(final int xLen, final int yLen, final Set<Piece> pieces, final Set<SquarePosition> obstacles) {
+        if (xLen < 0 || yLen < 0) {
+            throw new IllegalArgumentException("the board length argument must be greater then 0");
+        }
+
+        this.squareList = new ArrayList<>();
+    }
 
     @Override
     public Square getSquare(SquarePosition pos) {
