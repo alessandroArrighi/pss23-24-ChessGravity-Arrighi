9bad325bcb32833f12edbb7ab0e05a2ff00a2ed4
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-01 10:16:44 +0200

    add BoardImpl class

---
+++ src/main/java/it/unibo/chessgravity/model/impl/BoardImp.java
@@ -0,0 +1,20 @@
+package it.unibo.chessgravity.model.impl;
+
+import it.unibo.chessgravity.model.api.*;
+
+/**
+ * Implementazione dell'interfaccia Board.
+ * La classe funge da contenitore logico per la raccolta di tutte
+ * le istanze di Square che comporrano la scacchiera. Qui ne
+ * avviene la creazione e tutta la loro gestione.
+ */
+
+public class BoardImp implements Board {
+
+    @Override
+    public Square getSquare(SquarePosition pos) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getSquare'");
+    }
+    
+}
