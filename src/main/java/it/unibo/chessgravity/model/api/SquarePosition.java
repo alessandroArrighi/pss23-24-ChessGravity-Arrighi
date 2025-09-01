25ea7b2e33bff24bc984c0eba1bb0753d5d9d26c
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-01 09:39:05 +0200

    add SquarePosition constructor

--- src/main/java/it/unibo/chessgravity/model/api/SquarePosition.java
+++ src/main/java/it/unibo/chessgravity/model/api/SquarePosition.java
@@ -4,10 +4,14 @@ package it.unibo.chessgravity.model.api;
  * Classe che incapsula le informazioni di una posizione nella scacchiera.
  * Ogni istanza è costituita da una posizione sull'asse delle <<x>> e una
  * sull'asse delle <<y>>.
- * Una posizione per essere ritenuta valida, deve avere valori per entrambi
- * gli assi compresi tra 1 e n (larghezza della scacchiera) inclusi: [1, n]
  */
 
 public class SquarePosition {
-    
+    private final int posX;
+    private final int posY;
+
+    public SquarePosition(final int posX, final int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
 }
