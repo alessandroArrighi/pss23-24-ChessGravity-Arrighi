82716657d89985f07b5d6450b69d148c37655212
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-02 14:54:27 +0200

    update BaseMove interface

--- src/main/java/it/unibo/chessgravity/model/api/move/BaseMove.java
+++ src/main/java/it/unibo/chessgravity/model/api/move/BaseMove.java
@@ -1,9 +1,16 @@
 package it.unibo.chessgravity.model.api.move;
 
+import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
 /**
  * Interface mapping the whole base move that one piece can do. 
  * All the pieces makes a move that is composed with a 
  * combination of these types of moves.
  */
 
-public interface BaseMove extends MoveStrategy { }
+public interface BaseMove {
+    public static final int STEP = 1;
+
+    SquarePosition move(SquarePosition start, Board board);
+}
