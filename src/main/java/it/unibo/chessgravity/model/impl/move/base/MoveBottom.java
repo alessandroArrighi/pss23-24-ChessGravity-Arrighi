220c2daeeb8fb667431d195584342981c272030f
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-02 11:06:06 +0200

    add MoveBottom class

---
+++ src/main/java/it/unibo/chessgravity/model/impl/move/base/MoveBottom.java
@@ -0,0 +1,19 @@
+package it.unibo.chessgravity.model.impl.move.base;
+
+import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.move.BaseMove;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Class that models the base movement in direction of the bottom
+ * of the square.
+ */
+public class MoveBottom implements BaseMove {
+
+    @Override
+    public boolean move(SquarePosition start, SquarePosition dest, Board board) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
+    
+}
