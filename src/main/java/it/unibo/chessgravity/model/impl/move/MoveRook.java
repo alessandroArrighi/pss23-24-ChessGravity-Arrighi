4d11a1b82d73111ad563c67518642ef8faca2149
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-02 16:16:20 +0200

    add MoveRook class

---
+++ src/main/java/it/unibo/chessgravity/model/impl/move/MoveRook.java
@@ -0,0 +1,17 @@
+package it.unibo.chessgravity.model.impl.move;
+
+import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.move.MoveStrategy;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Class that models the movement strategy that has to be done by a rook piece.
+ */
+public class MoveRook implements MoveStrategy {
+
+    @Override
+    public boolean move(SquarePosition start, SquarePosition dest, Board board) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
+}
