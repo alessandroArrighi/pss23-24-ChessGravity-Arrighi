5941d29ca0e8810ba600b8ae14ed3c9e054f9988
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-02 17:21:46 +0200

    add SquarePositions class

---
+++ src/main/java/it/unibo/chessgravity/model/utils/SquarePositions.java
@@ -0,0 +1,11 @@
+package it.unibo.chessgravity.model.utils;
+
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Utility class that handle all the position comparation between two
+ * istances of the SquarePosition class.
+ */
+public final class SquarePositions {
+    private SquarePositions() { }
+}
