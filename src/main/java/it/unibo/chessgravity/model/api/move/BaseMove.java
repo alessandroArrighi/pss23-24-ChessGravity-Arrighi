54e6c528ffb9c21f67da47e78c7ba027dd7887c8
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-02 11:03:16 +0200

    add move interfaces

---
+++ src/main/java/it/unibo/chessgravity/model/api/move/BaseMove.java
@@ -0,0 +1,9 @@
+package it.unibo.chessgravity.model.api.move;
+
+/**
+ * Interface mapping the whole base move that one piece can do. 
+ * All the pieces makes a move that is composed with a 
+ * combination of these types of moves.
+ */
+
+public interface BaseMove extends MoveStrategy { }
---
+++ src/main/java/it/unibo/chessgravity/model/api/move/MovePiece.java
@@ -0,0 +1,7 @@
+package it.unibo.chessgravity.model.api.move;
+
+/**
+ * Interface mapping the specific moves that one piece has to do.
+ */
+
+public interface MovePiece extends MoveStrategy { }
