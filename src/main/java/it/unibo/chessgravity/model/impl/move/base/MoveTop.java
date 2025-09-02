8fcf60473169034dbf122dc40b6c92adae819b28
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-02 16:03:50 +0200

    add MoveTop class

---
+++ src/main/java/it/unibo/chessgravity/model/impl/move/base/MoveTop.java
@@ -0,0 +1,14 @@
+package it.unibo.chessgravity.model.impl.move.base;
+
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Class that implement the BaseMoveAbstract template method for the top movement.
+ */
+public class MoveTop extends BaseMoveAbstract {
+
+    @Override
+    protected SquarePosition calculatePos(int posX, int posY) {
+        return new SquarePosition(posX, posY + STEP);
+    }
+}
