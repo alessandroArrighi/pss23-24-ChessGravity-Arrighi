e2c3f63d4c0289acfc3365c4821b03bb122dc114
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-02 14:56:08 +0200

    add move implementation in MoveBottom class

--- src/main/java/it/unibo/chessgravity/model/impl/move/base/MoveBottom.java
+++ src/main/java/it/unibo/chessgravity/model/impl/move/base/MoveBottom.java
@@ -1,7 +1,9 @@
 package it.unibo.chessgravity.model.impl.move.base;
 
 import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.exceptions.IllegalSquarePositionException;
 import it.unibo.chessgravity.model.api.move.BaseMove;
+import it.unibo.chessgravity.model.api.square.Square;
 import it.unibo.chessgravity.model.api.square.SquarePosition;
 
 /**
@@ -9,11 +11,23 @@ import it.unibo.chessgravity.model.api.square.SquarePosition;
  * of the square.
  */
 public class MoveBottom implements BaseMove {
-
     @Override
-    public boolean move(SquarePosition start, SquarePosition dest, Board board) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    public SquarePosition move(SquarePosition start, Board board) {
+        // calculate new position
+        SquarePosition pos = new SquarePosition(start.getPosX(), start.getPosY() - STEP);
+
+        try {
+            Square square = board.getSquare(pos);
+
+            if (square.isFree()) {
+                return pos;
+            }
+        } catch (IllegalSquarePositionException e) { }
+
+        /*
+         * return null if the getSquare method throws IllegalSquarePositionException
+         * or the square to move the piece is not free
+         */
+        return null;
     }
-    
 }
