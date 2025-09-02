d7a3159609044010957a04063f47a7dac37d273c
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-02 10:14:19 +0200

    add MoveStrategy interface

---
+++ src/main/java/it/unibo/chessgravity/model/api/move/MoveStrategy.java
@@ -0,0 +1,12 @@
+package it.unibo.chessgravity.model.api.move;
+
+import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Strategy interface for the Moves algorithms of the pieces.
+ */
+
+public interface MoveStrategy {
+    boolean move(SquarePosition start, SquarePosition dest, Board board);
+}
