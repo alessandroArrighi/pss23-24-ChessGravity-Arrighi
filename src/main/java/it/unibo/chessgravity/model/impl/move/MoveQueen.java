80a79eaf7b48753afad8de810049e3e1a383f59d
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-03 18:42:58 +0200

    add MoveQueen class

---
+++ src/main/java/it/unibo/chessgravity/model/impl/move/MoveQueen.java
@@ -0,0 +1,16 @@
+package it.unibo.chessgravity.model.impl.move;
+
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+import it.unibo.chessgravity.model.impl.move.base.BaseMoveAbstract;
+
+/**
+ * Class that models the movement strategy that has to be done by a queen piece.
+ */
+public class MoveQueen extends BaseMoveAbstract {
+
+    @Override
+    protected SquarePosition calculatePos(int posX, int posY) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculatePos'");
+    }
+}
