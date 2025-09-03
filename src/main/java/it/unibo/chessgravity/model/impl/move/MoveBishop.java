4192a0145c95165070d072be404f6da61731c7af
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-03 18:33:04 +0200

    add MoveBishop class

---
+++ src/main/java/it/unibo/chessgravity/model/impl/move/MoveBishop.java
@@ -0,0 +1,17 @@
+package it.unibo.chessgravity.model.impl.move;
+
+import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.move.MovePiece;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Class that models the movement strategy that has to be done by a bishop piece.
+ */
+public class MoveBishop implements MovePiece {
+    
+    @Override
+    public boolean move(SquarePosition start, SquarePosition dest, Board board) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
+}
