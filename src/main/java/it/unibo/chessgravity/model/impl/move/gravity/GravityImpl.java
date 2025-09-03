7850c9f678ec623d3908619e710bb5b86881d98c
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-03 09:58:54 +0200

    add constructor

--- src/main/java/it/unibo/chessgravity/model/impl/move/gravity/GravityImpl.java
+++ src/main/java/it/unibo/chessgravity/model/impl/move/gravity/GravityImpl.java
@@ -1,8 +1,10 @@
 package it.unibo.chessgravity.model.impl.move.gravity;
 
 import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.move.BaseMove;
 import it.unibo.chessgravity.model.api.move.Gravity;
 import it.unibo.chessgravity.model.api.square.SquarePosition;
+import it.unibo.chessgravity.model.impl.move.base.MoveBottom;
 
 /**
  * class that models the gravity movement. This class will calculate the lowest
@@ -10,6 +12,11 @@ import it.unibo.chessgravity.model.api.square.SquarePosition;
  */
 
 public class GravityImpl implements Gravity {
+    private final BaseMove moveBottom;
+
+    public GravityImpl() {
+        moveBottom = new MoveBottom();
+    }
 
     @Override
     public SquarePosition gravity(SquarePosition start, Board board) {
