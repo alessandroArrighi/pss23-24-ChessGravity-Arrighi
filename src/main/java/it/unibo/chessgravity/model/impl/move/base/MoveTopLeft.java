020ba695fe5be73a729d0868fa83cadd9f9dce53
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-03 18:03:21 +0200

    add MoveTopLeft class

---
+++ src/main/java/it/unibo/chessgravity/model/impl/move/base/MoveTopLeft.java
@@ -0,0 +1,16 @@
+package it.unibo.chessgravity.model.impl.move.base;
+
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Class that implement the BaseMoveAbstract template method for the top left movement.
+ */
+public class MoveTopLeft extends BaseMoveAbstract {
+
+    @Override
+    protected SquarePosition calculatePos(int posX, int posY) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'calculatePos'");
+    }
+    
+}
