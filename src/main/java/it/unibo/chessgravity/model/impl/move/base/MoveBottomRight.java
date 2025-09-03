bfb5688eeed2cfc21914cc7799ba4e0d7175ac36
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-03 18:10:36 +0200

    add MoveBottomRight class

---
+++ src/main/java/it/unibo/chessgravity/model/impl/move/base/MoveBottomRight.java
@@ -0,0 +1,14 @@
+package it.unibo.chessgravity.model.impl.move.base;
+
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Class that implement the BaseMoveAbstract template method for the bottom right movement.
+ */
+public class MoveBottomRight extends BaseMoveAbstract {
+
+    @Override
+    protected SquarePosition calculatePos(int posX, int posY) {
+        return new SquarePosition(posX + STEP, posY - STEP);
+    }    
+}
