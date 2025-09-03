3ea2d32199179315686c28c38438ee2a987a1825
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-03 18:53:42 +0200

    add move method implementation

--- src/main/java/it/unibo/chessgravity/model/impl/move/MoveQueen.java
+++ src/main/java/it/unibo/chessgravity/model/impl/move/MoveQueen.java
@@ -3,6 +3,7 @@ package it.unibo.chessgravity.model.impl.move;
 import it.unibo.chessgravity.model.api.Board;
 import it.unibo.chessgravity.model.api.move.MovePiece;
 import it.unibo.chessgravity.model.api.square.SquarePosition;
+import static it.unibo.chessgravity.model.utils.SquarePositions.*;
 
 /**
  * Class that models the movement strategy that has to be done by a queen piece.
@@ -18,7 +19,18 @@ public class MoveQueen implements MovePiece {
 
     @Override
     public boolean move(SquarePosition start, SquarePosition dest, Board board) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'move'");
+        MovePiece move = null;
+
+        if (onSameColumn(start, dest) || onSameRow(start, dest)) {
+            move = moveRook;
+        } else if (onSameDiagonal(start, dest)) {
+            move = moveBishop;
+        }
+
+        if (move == null) {
+            return false;
+        }
+
+        return move.move(start, dest, board);
     }
 }
