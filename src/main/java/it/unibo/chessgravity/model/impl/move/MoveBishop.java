a6e7fc5af02b3870533f81268507fb75d9a4582a
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-03 18:34:51 +0200

    add MoveBishop constructor

--- src/main/java/it/unibo/chessgravity/model/impl/move/MoveBishop.java
+++ src/main/java/it/unibo/chessgravity/model/impl/move/MoveBishop.java
@@ -1,13 +1,26 @@
 package it.unibo.chessgravity.model.impl.move;
 
 import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.move.BaseMove;
 import it.unibo.chessgravity.model.api.move.MovePiece;
 import it.unibo.chessgravity.model.api.square.SquarePosition;
+import it.unibo.chessgravity.model.impl.move.base.*;
 
 /**
  * Class that models the movement strategy that has to be done by a bishop piece.
  */
 public class MoveBishop implements MovePiece {
+    private final BaseMove moveTopLeft;
+    private final BaseMove moveTopRight;
+    private final BaseMove moveBottomLeft;
+    private final BaseMove moveBottomRight;
+
+    public MoveBishop() {
+        moveTopLeft = new MoveTopLeft();
+        moveTopRight = new MoveTopRight();
+        moveBottomLeft = new MoveBottomLeft();
+        moveBottomRight = new MoveBottomRight();
+    }
     
     @Override
     public boolean move(SquarePosition start, SquarePosition dest, Board board) {
