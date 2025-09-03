effdef5f75c3dc2762a415c95397c246dcb0d6a9
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-03 09:57:51 +0200

    add GravityImpl class

--- src/main/java/it/unibo/chessgravity/model/api/move/Gravity.java
+++ src/main/java/it/unibo/chessgravity/model/api/move/Gravity.java
@@ -8,5 +8,5 @@ import it.unibo.chessgravity.model.api.square.SquarePosition;
  */
 
 public interface Gravity {
-    SquarePosition gravity(SquarePosition start, SquarePosition dest, Board board);
+    SquarePosition gravity(SquarePosition start, Board board);
 }
---
+++ src/main/java/it/unibo/chessgravity/model/impl/move/gravity/GravityImpl.java
@@ -0,0 +1,20 @@
+package it.unibo.chessgravity.model.impl.move.gravity;
+
+import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.move.Gravity;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * class that models the gravity movement. This class will calculate the lowest
+ * position of the board that a piece has to be palced.
+ */
+
+public class GravityImpl implements Gravity {
+
+    @Override
+    public SquarePosition gravity(SquarePosition start, Board board) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'gravity'");
+    }
+    
+}
