b731b21507bf35ff27ba6576ad17a9aba1159a67
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-03 09:54:25 +0200

    add Gravity interface

---
+++ src/main/java/it/unibo/chessgravity/model/api/move/Gravity.java
@@ -0,0 +1,12 @@
+package it.unibo.chessgravity.model.api.move;
+
+import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * interface that maps a gravity movement
+ */
+
+public interface Gravity {
+    SquarePosition gravity(SquarePosition start, SquarePosition dest, Board board);
+}
