9bd6992b9f8b8c551d6dd29d0d220b476d0e3aed
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-03 08:59:48 +0200

    add same diagonal cheks method

--- src/main/java/it/unibo/chessgravity/model/utils/SquarePositions.java
+++ src/main/java/it/unibo/chessgravity/model/utils/SquarePositions.java
@@ -25,32 +25,39 @@ public final class SquarePositions {
     }
 
     // true if a and b has thee same posX value
-    public static boolean sameColumn(SquarePosition a, SquarePosition b) {
+    public static boolean onSameColumn(SquarePosition a, SquarePosition b) {
         return Integer.compare(a.getPosX(), b.getPosX()) == 0;
     }
 
     // true if a and b has the same posY value
-    public static boolean sameRow(SquarePosition a, SquarePosition b) {
+    public static boolean onSameRow(SquarePosition a, SquarePosition b) {
         return Integer.compare(a.getPosY(), b.getPosY()) == 0;
     }
 
     // true if a and b are on the same column and posY a is grather than posY b
     public static boolean onTopColumn(SquarePosition a, SquarePosition b) {
-        return sameColumn(a, b) && gratherOrEqualY(a, b);
+        return onSameColumn(a, b) && gratherOrEqualY(a, b);
     }
 
     // true if both positions are on the same column and the first 
     public static boolean onBottomColumn(SquarePosition a, SquarePosition b) {
-        return sameColumn(a, b) && gratherOrEqualY(b, a);
+        return onSameColumn(a, b) && gratherOrEqualY(b, a);
     }
 
     // true if both are on the same row and a posX is grather than b posX
     public static boolean onLeftRow(SquarePosition a, SquarePosition b) {
-        return sameRow(a, b) && gratherOrEqualX(a, b);
+        return onSameRow(a, b) && gratherOrEqualX(a, b);
     }
 
     // true if both are on the same row and a posX is smaller than b posX
     public static boolean onRightRow(SquarePosition a, SquarePosition b) {
-        return sameRow(a, b) && gratherOrEqualX(b, a);
+        return onSameRow(a, b) && gratherOrEqualX(b, a);
+    }
+
+    public static boolean onSameDiagonal(SquarePosition a, SquarePosition b) {
+        final int xDelta = Math.abs(a.getPosX() - b.getPosX());
+        final int yDelta = Math.abs(a.getPosY() - b.getPosY());
+
+        return xDelta == yDelta;
     }
 }
