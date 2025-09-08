9a721e440812f4e439e70fd3641a4ff5a4ef645a
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-08 16:01:19 +0200

    add MoveKnight constructor

--- src/main/java/it/unibo/chessgravity/model/impl/move/MoveKnight.java
+++ src/main/java/it/unibo/chessgravity/model/impl/move/MoveKnight.java
@@ -1,13 +1,31 @@
 package it.unibo.chessgravity.model.impl.move;
 
 import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.move.BaseMove;
 import it.unibo.chessgravity.model.api.move.MovePiece;
 import it.unibo.chessgravity.model.api.square.SquarePosition;
+import it.unibo.chessgravity.model.impl.move.base.BaseMoveAbstract.MoveChecker;
+import it.unibo.chessgravity.model.impl.move.base.*;
 
 /**
  * Class that models the movement strategy that has to be done by a knight piece.
  */
 public class MoveKnight implements MovePiece {
+
+    private final BaseMove moveTop;
+    private final BaseMove moveLeft;
+    private final BaseMove moveRight;
+    private final BaseMove moveBottom;
+
+    public MoveKnight() {
+        final MoveChecker unCheck = (a, b) -> true;
+        
+        moveTop = new MoveTop(unCheck);
+        moveLeft = new MoveLeft(unCheck);
+        moveRight = new MoveRight(unCheck);
+        moveBottom = new MoveBottom(unCheck);
+    }
+
 
     @Override
     public boolean move(SquarePosition start, SquarePosition dest, Board board) {
