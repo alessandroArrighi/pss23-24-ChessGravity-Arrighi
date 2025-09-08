437553048e7ab0c5260738ae4ca0e534b384e764
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-08 17:29:17 +0200

    add MapImpl class

---
+++ src/main/java/it/unibo/chessgravity/model/impl/MapImpl.java
@@ -0,0 +1,16 @@
+package it.unibo.chessgravity.model.impl;
+
+import it.unibo.chessgravity.model.api.Map;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Class that models a map of the game.
+ */
+public class MapImpl implements Map {
+
+    @Override
+    public boolean move(SquarePosition start, SquarePosition dest) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
+}
