5005ce98fe58fc789232b9ee2192b84bfe23485c
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-11 15:58:06 +0200

    add GravityObservable interface

---
+++ src/main/java/it/unibo/chessgravity/model/api/GravityObservable.java
@@ -0,0 +1,19 @@
+package it.unibo.chessgravity.model.api;
+
+import java.util.Collection;
+
+/**
+ * Observable interface that represents the subject being observed
+ * by a GravityObserver
+ */
+public interface GravityObservable {
+    void subscribe(GravityObserver observer);
+
+    void unsubsribe(GravityObserver observer);
+
+    void subscribeAll(Collection<GravityObserver> observer);
+
+    void unsubsribeAll();
+
+    void notifyObservers();
+}
