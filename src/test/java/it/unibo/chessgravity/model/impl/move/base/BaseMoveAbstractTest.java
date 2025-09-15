3380b38a2a174fd8675fbdb2e2264c9bc68e5f8b
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-15 11:32:02 +0200

    add MoveMock class

--- src/test/java/it/unibo/chessgravity/model/impl/move/base/BaseMoveAbstractTest.java
+++ src/test/java/it/unibo/chessgravity/model/impl/move/base/BaseMoveAbstractTest.java
@@ -9,6 +9,26 @@ import it.unibo.chessgravity.model.api.square.SquarePosition;
  */
 public class BaseMoveAbstractTest {
 
+    /**
+     * Movement mock class.
+     * 
+     * This class implement the mock methods of the BaseMoveAbstract class.
+     */
+    public class MoveMock extends BaseMoveAbstract {
+
+        private final SquarePosition pos;
+
+        public MoveMock(final SquarePosition pos) {
+            super();
+            this.pos = pos;
+        }
+
+        @Override
+        protected SquarePosition calculatePos(int posX, int posY) {
+            return this.pos;
+        }
+    }
+
     private int LEN;
     private Board board;
     private SquarePosition startPos;
