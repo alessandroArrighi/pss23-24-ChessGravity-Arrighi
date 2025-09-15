2188a59796d17e5b9f82af0f9aabb0959bd0f107
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-15 18:03:00 +0200

    add setup

--- src/test/java/it/unibo/chessgravity/model/impl/BoardImplTest.java
+++ src/test/java/it/unibo/chessgravity/model/impl/BoardImplTest.java
@@ -1,6 +1,10 @@
 package it.unibo.chessgravity.model.impl;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
 
 import it.unibo.chessgravity.model.api.Board;
 import it.unibo.chessgravity.model.api.Piece;
@@ -17,4 +21,20 @@ public class BoardImplTest {
     private Piece piece1;
     private Piece piece2;
 
+    @BeforeEach
+    void setup() throws Exception {
+        obs = new HashSet<>();
+        obs.addAll(Arrays.asList(
+            new SquarePosition(3, 3),
+            new SquarePosition(5, 5),
+            new SquarePosition(8, 2)
+        ));
+        piece1 = new PieceImpl(board, new SquarePosition(2, 2), null, null);
+        piece2 = new PieceImpl(board, new SquarePosition(3, 6), null, null);
+
+        board = new BoardImpl(LEN, LEN, obs);
+
+        board.setPiece(piece1);
+        board.setPiece(piece2);
+    }
 }
