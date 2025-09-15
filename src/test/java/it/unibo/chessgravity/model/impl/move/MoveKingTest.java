073781503158fe88fa2c82a5965e488bb6aed481
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-15 11:58:04 +0200

    add setup

--- src/test/java/it/unibo/chessgravity/model/impl/move/MoveKingTest.java
+++ src/test/java/it/unibo/chessgravity/model/impl/move/MoveKingTest.java
@@ -1,10 +1,15 @@
 package it.unibo.chessgravity.model.impl.move;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 
 import it.unibo.chessgravity.model.api.Board;
 import it.unibo.chessgravity.model.api.move.MovePiece;
 import it.unibo.chessgravity.model.api.square.SquarePosition;
+import it.unibo.chessgravity.model.impl.BoardImpl;
 
 /**
  * Test class for {@link MoveKing} class
@@ -28,4 +33,23 @@ public class MoveKingTest {
     private SquarePosition bottom;
     private SquarePosition bottoRight;
 
+    @BeforeEach
+    void setup() {
+        move = new MoveKing();
+        board = new BoardImpl(LEN, LEN, new HashSet<>());
+        start = new SquarePosition(5, 5);
+        posX = start.getPosX();
+        posY = start.getPosY();
+        dest = new ArrayList<>();
+
+        // init moves positon
+        topLeft = new SquarePosition(posX - step, posY + step);
+        top = new SquarePosition(posX, posY + step);
+        topRight = new SquarePosition(posX + step, posY + step);
+        left = new SquarePosition(posX - step, posY);
+        right = new SquarePosition(posX + step, posY);
+        bottomLeft = new SquarePosition(posX - step, posY - step);
+        bottom = new SquarePosition(posX, posY - step);
+        bottoRight = new SquarePosition(posX + step, posY - step);
+    }
 }
