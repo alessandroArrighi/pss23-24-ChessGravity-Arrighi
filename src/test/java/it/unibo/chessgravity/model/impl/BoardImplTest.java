13728f3a2c8fa5d8f10c019ae5a9c8fdb12af0d1
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-15 17:57:41 +0200

    add BoardImplTest class

---
+++ src/test/java/it/unibo/chessgravity/model/impl/BoardImplTest.java
@@ -0,0 +1,20 @@
+package it.unibo.chessgravity.model.impl;
+
+import java.util.Set;
+
+import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.Piece;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Test class for {@link BoardImpl} class
+ */
+public class BoardImplTest {
+    
+    private final int LEN = 10;
+    private Board board;
+    private Set<SquarePosition> obs;
+    private Piece piece1;
+    private Piece piece2;
+
+}
