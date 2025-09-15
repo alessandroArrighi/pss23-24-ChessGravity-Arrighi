b0e20c02c430bc8cf270a0b73a07f146fa1962fb
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-15 17:16:37 +0200

    add setup

--- src/test/java/it/unibo/chessgravity/model/impl/move/MoveBishopTest.java
+++ src/test/java/it/unibo/chessgravity/model/impl/move/MoveBishopTest.java
@@ -1,10 +1,15 @@
 package it.unibo.chessgravity.model.impl.move;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 
 import it.unibo.chessgravity.model.api.Board;
 import it.unibo.chessgravity.model.api.move.MovePiece;
 import it.unibo.chessgravity.model.api.square.SquarePosition;
+import it.unibo.chessgravity.model.impl.BoardImpl;
 
 /**
  * Test class for {@link MoveBishop} class.
@@ -16,4 +21,14 @@ public class MoveBishopTest {
     private Board board;
     private SquarePosition start;
     private List<SquarePosition> dest;
+
+    @BeforeEach
+    void setup() {
+        move = new MoveBishop();
+        board = new BoardImpl(LEN, LEN, new HashSet<>());
+        start = new SquarePosition(5, 5);
+        dest = new ArrayList<>();
+    }
+
+    
 }
