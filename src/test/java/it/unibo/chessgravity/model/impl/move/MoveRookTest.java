73ea5ab7b54d677f06bca8d5f0e8e622143bf674
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-15 12:44:28 +0200

    add setup

--- src/test/java/it/unibo/chessgravity/model/impl/move/MoveRookTest.java
+++ src/test/java/it/unibo/chessgravity/model/impl/move/MoveRookTest.java
@@ -1,10 +1,15 @@
 package it.unibo.chessgravity.model.impl.move;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 
 import it.unibo.chessgravity.model.api.Board;
 import it.unibo.chessgravity.model.api.move.MovePiece;
 import it.unibo.chessgravity.model.api.square.SquarePosition;
+import it.unibo.chessgravity.model.impl.BoardImpl;
 
 /**
  * Test class the {@link MoveRook} class
@@ -18,4 +23,15 @@ public class MoveRookTest {
     private List<SquarePosition> dest;
     private int posX;
     private int posY;
+
+    @BeforeEach
+    void setup() {
+        move = new MoveRook();
+        board = new BoardImpl(LEN, LEN, new HashSet<>());
+        start = new SquarePosition(5, 5);
+        dest = new ArrayList<>();
+        posX = start.getPosX();
+        posY = start.getPosY();
+    }
+
 }
