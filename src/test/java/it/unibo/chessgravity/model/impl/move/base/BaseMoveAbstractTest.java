fc7033a1cb26c6f18bad436ccb4d7994c3d10f59
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-15 11:28:45 +0200

    add BaseMoveAbstractTest class

---
+++ src/test/java/it/unibo/chessgravity/model/impl/move/base/BaseMoveAbstractTest.java
@@ -0,0 +1,17 @@
+package it.unibo.chessgravity.model.impl.move.base;
+
+import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.move.BaseMove;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Test class for BaseMoveAbstract class.
+ */
+public class BaseMoveAbstractTest {
+
+    private int LEN;
+    private Board board;
+    private SquarePosition startPos;
+    private SquarePosition endPos;
+    private BaseMove move;
+}
