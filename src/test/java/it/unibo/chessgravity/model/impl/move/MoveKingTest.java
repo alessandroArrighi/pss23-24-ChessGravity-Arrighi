9f8aa2343aca10e8d82727ed4cfddb18661d3faa
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-15 12:27:41 +0200

    add collisionMove

--- src/test/java/it/unibo/chessgravity/model/impl/move/MoveKingTest.java
+++ src/test/java/it/unibo/chessgravity/model/impl/move/MoveKingTest.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ import it.unibo.chessgravity.model.api.Board;
 import it.unibo.chessgravity.model.api.move.MovePiece;
 import it.unibo.chessgravity.model.api.square.SquarePosition;
 import it.unibo.chessgravity.model.impl.BoardImpl;
+import it.unibo.chessgravity.model.impl.PieceImpl;
 
 /**
  * Test class for {@link MoveKing} class
@@ -110,5 +112,24 @@ public class MoveKingTest {
         for (SquarePosition pos : dest) {
             assertFalse(move.move(start, pos, board));
         }
+    }
+
+    /*
+     * Utility method for cheks if a collision works correctly. This provides a template
+     * to use for each type of movement
+     */
+    private void collisionMove(final SquarePosition collision) throws Exception {
+        Set<SquarePosition> obs = new HashSet<>();
+        obs.add(collision);
+        board = new BoardImpl(LEN, LEN, obs);
+
+        // Test collision with an obstacle
+        assertFalse(move.move(start, collision, board));
+
+        board = new BoardImpl(LEN, LEN, new HashSet<>());
+        board.setPiece(new PieceImpl(board, collision, move, null));
+
+        // Test collision with a piece
+        assertFalse(move.move(start, collision, board));
     }
 }
