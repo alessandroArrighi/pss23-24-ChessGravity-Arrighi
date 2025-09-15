b7b8a645bdc423fc1a5586ed23f6b58aa7575926
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-15 12:43:45 +0200

    add MoveRookTest class

---
+++ src/test/java/it/unibo/chessgravity/model/impl/move/MoveRookTest.java
@@ -0,0 +1,21 @@
+package it.unibo.chessgravity.model.impl.move;
+
+import java.util.List;
+
+import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.move.MovePiece;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Test class the {@link MoveRook} class
+ */
+public class MoveRookTest {
+    
+    private int LEN = 10;
+    private MovePiece move;
+    private Board board;
+    private SquarePosition start;
+    private List<SquarePosition> dest;
+    private int posX;
+    private int posY;
+}
