1003d4cf3dc6f3bc6eab61fffc89327c57589353
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-15 11:44:21 +0200

    add setup

--- src/test/java/it/unibo/chessgravity/model/impl/move/gravity/GravityImplTest.java
+++ src/test/java/it/unibo/chessgravity/model/impl/move/gravity/GravityImplTest.java
@@ -1,8 +1,13 @@
 package it.unibo.chessgravity.model.impl.move.gravity;
 
+import java.util.HashSet;
+
+import org.junit.jupiter.api.BeforeEach;
+
 import it.unibo.chessgravity.model.api.Board;
 import it.unibo.chessgravity.model.api.move.Gravity;
 import it.unibo.chessgravity.model.api.square.SquarePosition;
+import it.unibo.chessgravity.model.impl.BoardImpl;
 
 /**
  * Test class for the {@link GravityImpl} class
@@ -15,5 +20,11 @@ public class GravityImplTest {
     private Board board;
     private SquarePosition start;
     private SquarePosition dest;
+
+    @BeforeEach
+    void setup() {
+        gravity = new GravityImpl();
+        board = new BoardImpl(LEN, LEN, new HashSet<>());
+    }
 
 }
