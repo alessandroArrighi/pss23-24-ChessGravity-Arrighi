af17e9e445d211e7eef90741219217b2338d5fbe
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-15 17:15:58 +0200

    add MoveBishopTest class

---
+++ src/test/java/it/unibo/chessgravity/model/impl/move/MoveBishopTest.java
@@ -0,0 +1,19 @@
+package it.unibo.chessgravity.model.impl.move;
+
+import java.util.List;
+
+import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.move.MovePiece;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Test class for {@link MoveBishop} class.
+ */
+public class MoveBishopTest {
+    
+    private int LEN = 10;
+    private MovePiece move;
+    private Board board;
+    private SquarePosition start;
+    private List<SquarePosition> dest;
+}
