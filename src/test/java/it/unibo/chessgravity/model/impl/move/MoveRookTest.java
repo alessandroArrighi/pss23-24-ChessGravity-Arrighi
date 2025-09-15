0cf17088a745d59a4b9facc9a92cfc1e3723e274
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-15 12:46:42 +0200

    add testSimpleMove

--- src/test/java/it/unibo/chessgravity/model/impl/move/MoveRookTest.java
+++ src/test/java/it/unibo/chessgravity/model/impl/move/MoveRookTest.java
@@ -1,10 +1,14 @@
 package it.unibo.chessgravity.model.impl.move;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import it.unibo.chessgravity.model.api.Board;
 import it.unibo.chessgravity.model.api.move.MovePiece;
@@ -32,6 +36,29 @@ public class MoveRookTest {
         dest = new ArrayList<>();
         posX = start.getPosX();
         posY = start.getPosY();
+    }
+
+    /**
+     * Checks if the move works correctly
+     */
+    @Test
+    void testSimpleMoves() {
+        dest.addAll(Arrays.asList(
+            // Move left
+            new SquarePosition(1, posY),
+            // Move right
+            new SquarePosition(1, posY),
+            // Move top
+            new SquarePosition(posX, 10),
+            // Move bottom
+            new SquarePosition(posX, 1),
+            // Don't move
+            start
+        ));
+        
+        for (SquarePosition pos : dest) {
+            assertTrue(move.move(start, pos, board));
+        }
     }
 
 }
