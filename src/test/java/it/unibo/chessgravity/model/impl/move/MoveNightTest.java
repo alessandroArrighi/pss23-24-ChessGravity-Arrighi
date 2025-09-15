8d0e2b496de193e09babdd0aa95110d10c86fc06
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-15 17:41:14 +0200

    add MoveNightTest class

---
+++ src/test/java/it/unibo/chessgravity/model/impl/move/MoveNightTest.java
@@ -0,0 +1,26 @@
+package it.unibo.chessgravity.model.impl.move;
+
+import java.util.List;
+
+import it.unibo.chessgravity.model.api.Board;
+import it.unibo.chessgravity.model.api.move.MovePiece;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Test class for {@link MoveNight} class
+ */
+public class MoveNightTest {
+
+    private int LEN = 10;
+    private MovePiece move;
+    private Board board;
+    private SquarePosition start;
+    private List<SquarePosition> dest;
+    private int posX;
+    private int posY;
+    private List<SquarePosition> topLeft;
+    private List<SquarePosition> topRight;
+    private List<SquarePosition> bottomLeft;
+    private List<SquarePosition> bottomRight;
+
+}
