361edafa72ccc4557c04f20dc4ad800328ab07ca
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-16 12:01:42 +0200

    add GravityObserverMock class

--- src/test/java/it/unibo/chessgravity/model/impl/GravityNotifierTest.java
+++ src/test/java/it/unibo/chessgravity/model/impl/GravityNotifierTest.java
@@ -1,8 +1,28 @@
 package it.unibo.chessgravity.model.impl;
 
+import it.unibo.chessgravity.model.api.GravityObserver;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
 /**
  * Test class for {@link GravityNotifier} class
  */
 public class GravityNotifierTest {
-    
+
+    /**
+     * Mock class for {@link GravityObserver} reference
+     */
+    public class GravityObserverMock implements GravityObserver {
+
+        @Override
+        public void gravity() {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'gravity'");
+        }
+
+        @Override
+        public SquarePosition getPos() {
+            // TODO Auto-generated method stub
+            throw new UnsupportedOperationException("Unimplemented method 'getPos'");
+        }
+    }
 }
