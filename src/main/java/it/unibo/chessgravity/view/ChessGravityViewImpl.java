126b6ed37229f5060449ddd251b6181b2f1bcf22
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-17 19:22:15 +0200

    add ChessGravityViewImpl class

---
+++ src/main/java/it/unibo/chessgravity/view/ChessGravityViewImpl.java
@@ -0,0 +1,27 @@
+package it.unibo.chessgravity.view;
+
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+import it.unibo.chessgravity.view.api.ChessGravityView;
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.shape.Rectangle;
+
+/**
+ * FXML Controller that handle the ChessGravity view
+ */
+public class ChessGravityViewImpl implements ChessGravityView {
+
+    @FXML
+    private Group squares;
+    
+    @FXML
+    private Group pieces;
+
+    private Rectangle move;
+
+    @Override
+    public void move(SquarePosition start, SquarePosition dest) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
+}
