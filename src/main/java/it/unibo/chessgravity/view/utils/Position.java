e0d3e224e5649b41609b62e7a7824ab0bc073b01
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-19 11:50:50 +0200

    add Position utility class

---
+++ src/main/java/it/unibo/chessgravity/view/utils/Position.java
@@ -0,0 +1,12 @@
+package it.unibo.chessgravity.view.utils;
+
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Utility class that models the position of a type {@link EntityView}
+ * 
+ * This also handle the conversion from and to {@link SquarePosition}
+ */
+public class Position {
+    
+}
