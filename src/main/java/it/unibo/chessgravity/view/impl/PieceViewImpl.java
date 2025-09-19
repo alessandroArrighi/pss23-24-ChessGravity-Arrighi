567ad186046adbfa32806340f52d08add3086082
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-19 11:54:56 +0200

    add PieceViewImpl class

---
+++ src/main/java/it/unibo/chessgravity/view/impl/PieceViewImpl.java
@@ -0,0 +1,31 @@
+package it.unibo.chessgravity.view.impl;
+
+import it.unibo.chessgravity.view.api.EntityView;
+import it.unibo.chessgravity.view.utils.Position;
+
+/**
+ * Entity class that implements the {@link EntityView} interface.
+ * 
+ * This class models a piece of the game.
+ */
+public class PieceViewImpl implements EntityView {
+
+    @Override
+    public void setSize(int size) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setSize'");
+    }
+
+    @Override
+    public Position getPosition() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPosition'");
+    }
+
+    @Override
+    public void setPosition(Position pos) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setPosition'");
+    }
+    
+}
