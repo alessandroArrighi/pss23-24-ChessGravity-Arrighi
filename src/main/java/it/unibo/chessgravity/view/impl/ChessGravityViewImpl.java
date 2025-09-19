29ebe8ff4dc01ebee97f63b2ab4fc58c29b03aa9
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-19 11:46:46 +0200

    add BoardView impl in ChessGravityViewImpl class

--- src/main/java/it/unibo/chessgravity/view/impl/ChessGravityViewImpl.java
+++ src/main/java/it/unibo/chessgravity/view/impl/ChessGravityViewImpl.java
@@ -1,15 +1,18 @@
 package it.unibo.chessgravity.view.impl;
 
 import it.unibo.chessgravity.model.api.square.SquarePosition;
+import it.unibo.chessgravity.view.api.BoardView;
 import it.unibo.chessgravity.view.api.ChessGravityView;
+import it.unibo.chessgravity.view.api.EntityView;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.shape.Rectangle;
 
 /**
  * FXML Controller that handle the ChessGravity view
  */
-public class ChessGravityViewImpl implements ChessGravityView {
+public class ChessGravityViewImpl implements ChessGravityView, BoardView {
 
     @FXML
     private Group squares;
@@ -17,11 +20,16 @@ public class ChessGravityViewImpl implements ChessGravityView {
     @FXML
     private Group pieces;
 
-    private Rectangle move;
+    private EntityView move;
 
     @Override
     public void move(SquarePosition start, SquarePosition dest) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
+
+    @Override
+    public void setMove(EntityView piece) {
+        this.move = piece;
     }
 }
