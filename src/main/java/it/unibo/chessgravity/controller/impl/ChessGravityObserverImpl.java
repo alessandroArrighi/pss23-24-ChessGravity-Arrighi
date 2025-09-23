c3658f2bf779f42e7c224576bbc7a655c3263e8a
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-23 15:59:08 +0200

    add ChessGravityObserverImpl class

---
+++ src/main/java/it/unibo/chessgravity/controller/impl/ChessGravityObserverImpl.java
@@ -0,0 +1,17 @@
+package it.unibo.chessgravity.controller.impl;
+
+import it.unibo.chessgravity.controller.api.ChessGravityObserver;
+import it.unibo.chessgravity.model.api.square.SquarePosition;
+
+/**
+ * Controller class that handle the interaction between the view and the model
+ */
+public class ChessGravityObserverImpl implements ChessGravityObserver {
+
+    @Override
+    public void move(SquarePosition start, SquarePosition dest) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    }
+    
+}
