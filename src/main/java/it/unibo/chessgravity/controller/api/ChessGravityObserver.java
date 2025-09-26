920b5dcb1279bf9378d0f2bff9608f6ea90ba643
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-26 18:30:14 +0200

    add javadocs for ChessGravityObserver interface

--- src/main/java/it/unibo/chessgravity/controller/api/ChessGravityObserver.java
+++ src/main/java/it/unibo/chessgravity/controller/api/ChessGravityObserver.java
@@ -6,6 +6,13 @@ import it.unibo.chessgravity.view.utils.Position;
  * Interface that maps the controller for binding the view with the model
  */
 public interface ChessGravityObserver {
+    /**
+     * Method used by the view to ask the model to handle
+     * a movement selected by the user.
+     * 
+     * @param start The start (current) position of the piece
+     * @param dest The destination position of the piece.
+     */
     void move(Position start, Position dest);
 
     void restart();
