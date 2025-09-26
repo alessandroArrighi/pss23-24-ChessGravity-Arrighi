c5cf2c2b82d1f4e5afbf702159a90c69817f7d39
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-26 19:40:19 +0200

    add javadocs to Map interface

--- src/main/java/it/unibo/chessgravity/model/api/Map.java
+++ src/main/java/it/unibo/chessgravity/model/api/Map.java
@@ -9,8 +9,23 @@ import it.unibo.chessgravity.model.utils.PieceSetting;
  * Interface mapping the map of the game.
  */
 public interface Map {
+    /**
+     * Method used to get a move request by the view and return the moved pieces.
+     * 
+     * @param start The starting position of the piece.
+     * @param dest The destination position.
+     * @return A list of {@link PieceSetting} objects. These are all the moved pieces by
+     * the gravity after the movement of the piece. This Method returns null if the game is
+     * over or if the movement is not possible to be done.
+     */
     List<PieceSetting> move(SquarePosition start, SquarePosition dest);
 
+    /**
+     * Method used to start the game.
+     * 
+     * @return A list of {@link PieceSetting} objects. These are the pieces sorted after the
+     * gravity calls.
+     */
     List<PieceSetting> start();
 
     boolean isGameOver();
