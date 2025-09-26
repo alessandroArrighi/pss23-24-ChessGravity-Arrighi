00908ef2437ca548dd448b8f90889f9dd37d4b0d
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-26 11:21:19 +0200

    add init method

--- src/main/java/it/unibo/chessgravity/view/impl/EnemyView.java
+++ src/main/java/it/unibo/chessgravity/view/impl/EnemyView.java
@@ -2,6 +2,7 @@ package it.unibo.chessgravity.view.impl;
 
 import it.unibo.chessgravity.view.utils.Position;
 import javafx.fxml.FXML;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 /**
@@ -18,5 +19,15 @@ public class EnemyView {
     public EnemyView(final Position pos, final int size) {
         this.pos = pos;
         this.size = size;
+    }
+
+    @FXML
+    public void initialize() {
+        piece.setWidth(size);
+        piece.setHeight(size);
+        piece.setX(pos.getPosX());
+        piece.setY(pos.getPosY());
+
+        piece.setFill(Color.RED);
     }
 }
