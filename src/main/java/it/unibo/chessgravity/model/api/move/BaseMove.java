294c82abd71ab680bc7482bed08884dcbf18db65
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-26 18:50:04 +0200

    add javadocs to BaseMove interface

--- src/main/java/it/unibo/chessgravity/model/api/move/BaseMove.java
+++ src/main/java/it/unibo/chessgravity/model/api/move/BaseMove.java
@@ -8,9 +8,24 @@ import it.unibo.chessgravity.model.api.square.SquarePosition;
  * All the pieces makes a move that is composed with a 
  * combination of these types of moves.
  */
-
 public interface BaseMove {
+    /**
+     * Method that handles the base movement calculation.
+     * 
+     * @param start The starting position.
+     * @param board The board used to checks the collisions.
+     * @return {@link MoveResponse} objects with all calculated results.
+     */
     MoveResponse move(SquarePosition start, Board board);
 
+    /**
+     * Same method as {@link BaseMove#move(SquarePosition, Board)} but with
+     * a different step.
+     * 
+     * @param start The starting position.
+     * @param step The step used for the calculations. If a collision entity 
+     * is between two steps, it will be ignored.
+     * @return {@link MoveResponse} objects with all calculated results.
+     */
     MoveResponse move(SquarePosition start, Board board, int step);
 }
