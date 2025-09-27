d88d0d3b99fcc6f0cc10f0e63da5f3597b483a17
Alessandro Arrighi <alessandro.arrighi2@studio.unibo.it>
2025-09-27 10:31:08 +0200

    add javadocs for Enemy

--- src/main/java/it/unibo/chessgravity/model/impl/Enemy.java
+++ src/main/java/it/unibo/chessgravity/model/impl/Enemy.java
@@ -5,12 +5,19 @@ import it.unibo.chessgravity.model.api.square.SquarePosition;
 /**
  * Class that models the enemy king piece. Unlike the other pieces, the enemy king
  * cannot be moved.
+ * When this class is created, it automatically sets the state of the king to 'alive'. 
+ * This state can only be changed once.
  */
 public class Enemy {
 
     private final SquarePosition pos;
     private boolean alive;
 
+    /**
+     * Class constructor
+     * 
+     * @param pos The position of the enemy. This cannot be changed.
+     */
     public Enemy(final SquarePosition pos) {
         this.pos = pos;
         alive = true;
@@ -20,6 +27,10 @@ public class Enemy {
         return alive;
     }
 
+    /**
+     * Method to 'kill' the enemy king. This method permanently changes
+     * the state of this instance.
+     */
     public void kill() {
         this.alive = false;
     }
